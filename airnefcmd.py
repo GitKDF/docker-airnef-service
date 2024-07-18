8bce3a396606538c7be9775da45ffb842231839f
GitKDF <39571876+GitKDF@users.noreply.github.com>
2024-07-17 22:44:57 -0600

    Update airnefcmd.py

--- airnefcmd.py
+++ airnefcmd.py
@@ -1086,8 +1086,8 @@ def ssdpDiscoverCameraIpAddress():
 	except ssdp.DiscoverFailureException as e:
 		# raise ssdp.DiscoverFailureException(">> Connection Failed <<\n\n" + str(e)) # prepend "Connection Failed" message to exception text
   		if not e.startswith("\nNo camera found."):
-    			e = ">> Connection Failed <<\n\n" + str(e)
-  		raise DiscoverFailureException(e)
+    			e = ">> Connection Failed <<\n\n" + e
+  		raise ssdp.DiscoverFailureException(e)
 	finally:
 		consoleClearLine()
 	
