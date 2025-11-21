15642be36e1c1268090019c32fd8dc76d426c3b2
GitKDF <39571876+GitKDF@users.noreply.github.com>
2025-11-21 00:16:11 -0700

    missing "

--- airnefcmd.py
+++ airnefcmd.py
@@ -1799,7 +1799,7 @@ def loadAndValidateMtpObjectInfoCacheFromDisk(objHandlesFromCameraList):
 		g.downloadHistoryDict = {}
 		if (os.path.exists(downloadHistoryFilename)):
 			downloadHistoryFilename = g.cameraLocalMetadataPathAndRootName + "-downloadhist";
-			applog_v("Deleting download history file \"{:s}\".format(downloadHistoryFilename))
+			applog_v("Deleting download history file \"{:s}\"".format(downloadHistoryFilename))
 			os.remove(downloadHistoryFilename)
 			fileDownloadHistory = open(downloadHistoryFilename, "a")
 		return None
